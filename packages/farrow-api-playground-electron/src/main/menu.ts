953d4c7ec9b5d9eff5c07428451ca52fcefd3f5f
Ma Tianqi <mtq1997@126.com>
2021-10-06 01:26:59 +0800

    fix: electron package error

--- packages/farrow-api-playground-electron/src/main/menu.ts
+++ packages/farrow-api-playground-electron/src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Farrow Playground',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Playground',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
